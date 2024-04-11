5fe3f45e93e356b7633bf747161a0507d1eb217e
MatheusFelipeCamarinhaDuarte <matheus.duarte.lins@gmail.com>
2024-04-11 16:02:48 -0300

    Feita a classe de entidade de Pessoa.

--- src/main/java/br/com/fiap/healy/entity/Pessoa.java
+++ src/main/java/br/com/fiap/healy/entity/Pessoa.java
@@ -1,4 +1,37 @@
 package br.com.fiap.healy.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "TB_HEALY_PESSOA", uniqueConstraints = {
+        @UniqueConstraint(name = "UK_TB_HEALY_PESSOA",columnNames = {
+                "EMAIL"
+        })
+})
 public class Pessoa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "SQ_HEALY_PESSOA")
+    @SequenceGenerator(name = "SQ_HEALY_PESSOA")
+    @Column(name = "ID_PESSOA")
+    private Long id;
+
+    @Column(name = "NM_PESSOA")
+    private String nome;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "TELEFONE")
+    private String telefone;
+
+    @Column(name = "DT_NASCIMENTO")
+    private LocalDate nasciemento;
+
 }
