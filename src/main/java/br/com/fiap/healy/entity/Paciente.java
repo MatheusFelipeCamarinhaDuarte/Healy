064fcc2512d34ccb96fe90798374ebcb66b0dce6
MatheusFelipeCamarinhaDuarte <matheus.duarte.lins@gmail.com>
2024-04-11 19:05:55 -0300

    Feita a classe de entidade de Paciente. Utilizamos ManyToOne para O plano de saude com paciente e pessoa com paciente.

--- src/main/java/br/com/fiap/healy/entity/Paciente.java
+++ src/main/java/br/com/fiap/healy/entity/Paciente.java
@@ -1,4 +1,53 @@
 package br.com.fiap.healy.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "TB_HEALY_PACIENTE")
 public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_HEALY_PACIENTE")
+    @SequenceGenerator(name = "SQ_HEALY_PACIENTE", sequenceName = "SQ_HEALY_PACIENTE", allocationSize = 1)
+    @Column(name = "ID_PACIENTE")
+    private Long id;
+
+    @Column(name = "USER_PACIENTE")
+    private String user;
+
+    @Column(name = "SENHA_PACIENTE")
+    private String senha;
+
+    @Column(name = "CPF")
+    private String cpf;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JoinColumn(
+            name = "PESSOA",
+            referencedColumnName = "ID_PESSOA",
+            foreignKey = @ForeignKey(
+                    name = "FK_PACIENTE_PESSOA"
+            )
+    )
+    private Pessoa pessoa;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = {CascadeType.MERGE,CascadeType.PERSIST})
+    @JoinColumn(
+            name = "PLANO",
+            referencedColumnName = "ID_PLANO_SAUDE",
+            foreignKey = @ForeignKey(
+                    name = "FK_PACIENTE_PLANO_SAUDE"
+            )
+    )
+    private PlanoSaude plano;
+
+
 }
