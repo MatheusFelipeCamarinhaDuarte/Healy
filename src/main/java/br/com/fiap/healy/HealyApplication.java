ea97007d2ad85ce405436d1a6cbe3111003febe8
MatheusFelipeCamarinhaDuarte <matheus.duarte.lins@gmail.com>
2024-04-11 14:29:50 -0300

    Criação das pastas dentro do projeto e aplicação das dependencys no Pom.xml, junto com a criação da pasta de documentação

---
+++ src/main/java/br/com/fiap/healy/HealyApplication.java
@@ -0,0 +1,12 @@
+package br.com.fiap.healy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run( HealyApplication.class, args );
+    }
+
+}
---
+++ src/test/java/br/com/fiap/healy/HealyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.healy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
