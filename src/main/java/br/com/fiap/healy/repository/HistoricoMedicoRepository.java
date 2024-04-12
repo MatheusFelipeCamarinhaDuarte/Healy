df0a35cdbe0deabdc8bb5097ae9d130d69e79662
MatheusFelipeCamarinhaDuarte <matheus.duarte.lins@gmail.com>
2024-04-12 03:24:27 -0300

    adicionado arquivo de HistoricoMedicoRepository.java

--- src/main/java/br/com/fiap/healy/repository/HistoricoMedicoRepository.java
+++ src/main/java/br/com/fiap/healy/repository/HistoricoMedicoRepository.java
@@ -1,9 +1,9 @@
 package br.com.fiap.healy.repository;
 
-import br.com.fiap.healy.entity.PlanoSaude;
+import br.com.fiap.healy.entity.HistoricoMedico;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PlanoSaudeRepository extends JpaRepository<PlanoSaude, Long> {
+public interface HistoricoMedicoRepository extends JpaRepository<HistoricoMedico, Long> {
 }
