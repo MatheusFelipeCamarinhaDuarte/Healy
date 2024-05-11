335dc32abecd65423f8582491aff82ebc728413c
MatheusFelipeCamarinhaDuarte <rm552295@fiap.com.br>
2024-05-10 21:34:54 -0300

    Classes de Repository refeitas

--- src/main/java/br/com/fiap/healy/repository/HistoricoMedicoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.healy.repository;
-
-import br.com.fiap.healy.entity.HistoricoMedico;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HistoricoMedicoRepository extends JpaRepository<HistoricoMedico, Long> {
-}
