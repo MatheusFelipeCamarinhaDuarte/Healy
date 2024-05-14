4bd7054766ddcbb901ee8cc13227cdb4a2e28cc2
MatheusFelipeCamarinhaDuarte <rm552295@fiap.com.br>
2024-05-13 22:34:35 -0300

    Realizadas as mudanças finais que exame precisa conter

--- src/main/java/br/com/fiap/healy/entity/Exame.java
+++ src/main/java/br/com/fiap/healy/entity/Exame.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -19,9 +21,51 @@ public class Exame {
     @Column(name = "ID_HISTORICO_MEDICO")
     private Long id;
 
-    // TODO: fazer exame uma classe funcional
-    @Column(name = "EXAME")
-    private String exame;
+    @Column(name = "SEXO")
+    private String sexo;
+    @Column(name = "IDADE")
+    private Integer idade;
+
+    @Column(name = "HIST_DIABETES")
+    private Boolean histDiabetes;
+    @Column(name = "HIST_DOENC_CORONARIA")
+    private Boolean histDoencaCoronaria;
+    @Column(name = "HIST_DOENC_VASCULAR")
+    private Boolean histDoencaVascular;
+    @Column(name = "HIST_FUMO")
+    private Boolean histFumo;
+    @Column(name = "HIST_HIPERTENSAO")
+    private Boolean histHipertensao;
+    @Column(name = "HIST_DISLIPIDEMIA")
+    private Boolean histDislipidemia;
+    @Column(name = "HIST_OBESIDADE")
+    private Boolean histObesidade;
+
+    @Column(name = "REMED_DISLIPIDEMIA")
+    private Boolean remDislipidemia;
+    @Column(name = "REMED_DIABETES")
+    private Boolean remDiabetes;
+    @Column(name = "REMED_HIPERTENSAO")
+    private Boolean remHipertensao;
+    @Column(name = "REMED_ACEI_ARB")
+    private Boolean remACEIeARB;
+
+    @Column(name = "NVL_COLESTEROL")
+    private Double nvlColesterol;
+    @Column(name = "NVL_CREATINA")
+    private Double nvlCreatina;
+    @Column(name = "EXAM_EGFRB")
+    private Double exameGFRB;
+    @Column(name = "PRES_SISTOLICA")
+    private Integer presSistolica;
+    @Column(name = "PRES_DIASTOLICA")
+    private Integer presDiastolica;
+    @Column(name = "INDC_MASSA_CORP")
+    private Integer indiceMassa;
+    @Column(name = "MESES_ATE_CRISE")
+    private Integer mesAteCrise;
+    @Column(name = "ANOS_ATE_CRISE" )
+    private Integer anoAteCrise;
 
     @ManyToOne(fetch = FetchType.EAGER, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
     @JoinColumn(
