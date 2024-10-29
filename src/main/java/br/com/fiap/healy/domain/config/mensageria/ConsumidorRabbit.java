64941a3287a3d40ee5d4f2c117a3eb97672d53f0
MatheusFelipeCamarinhaDuarte <rm552295@fiap.com.br>
2024-10-29 08:50:50 -0300

    adção das configurações de mensageria e internacionalização das mesmas

--- src/main/java/br/com/fiap/healy/domain/config/mensageria/ConsumidorRabbit.java
+++ src/main/java/br/com/fiap/healy/domain/config/mensageria/ConsumidorRabbit.java
@@ -1,2 +1,16 @@
-package br.com.fiap.healy.domain.config.mensageria;public class ConsumidorRabbit {
+package br.com.fiap.healy.domain.config.mensageria;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.ModelAndView;
+
+@Service
+public class ConsumidorRabbit {
+
+    @RabbitListener(queues = ConfigRabbitMQ.fila)
+    public void lerMensagem(String msg) {
+        ModelAndView mv = new ModelAndView("enviar_mensagem_rabbit");
+        System.out.println("Mensagem recebida: " + msg);
+    }
+
 }
--- src/main/java/br/com/fiap/healy/domain/config/mensageria/ProdutorRabbit.java
+++ src/main/java/br/com/fiap/healy/domain/config/mensageria/ProdutorRabbit.java
@@ -1,2 +1,21 @@
-package br.com.fiap.healy.domain.config.mensageria;public class ProdutorRabbit {
+package br.com.fiap.healy.domain.config.mensageria;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProdutorRabbit {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    public void enviarMensagem(String msg) {
+        rabbitTemplate.convertAndSend(ConfigRabbitMQ.roteador,
+                ConfigRabbitMQ.chave_rota,
+                msg);
+        System.out.println("Mensagem enviada: " + msg);
+
+    }
+
 }
